b63e7315baccaf3cf55752850ed7f7b39acf38c5
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-19 22:04:58 +0000

    requirement - selects all rollover students

---
+++ 1_queries/7_rollover_students.sql
@@ -0,0 +1,7 @@
+-- Select all rollover students
+
+SELECT students.name AS student_name, cohorts.name AS cohort_name, cohorts.start_date AS cohort_start_date, students.start_date AS student_start_date
+FROM students 
+JOIN cohorts ON (students.cohort_id = cohorts.id)
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
