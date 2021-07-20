c9e10c40bd80217e262b535ecdc1a9bf5e55408d
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-20 20:08:14 +0000

    each day with total number of assignments and duration

---
+++ 4_queries/11_total_assignments_and_duration.sql
@@ -0,0 +1,6 @@
+-- get each day with the total number of assignments and the total duration of the assignments
+
+SELECT day, COUNT(*) AS number_of_assignments, SUM(duration) AS duration
+FROM assignments
+GROUP BY day
+ORDER BY day;
