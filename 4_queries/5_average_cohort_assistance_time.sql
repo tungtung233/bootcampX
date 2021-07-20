295546a803c68ffea3d1c63c483c67642002ea25
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-20 18:58:02 +0000

    requirement - average duration of assistance requests by cohort

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,10 @@
+-- get the average duration of assistance requests for each cohort
+
+
+SELECT cohorts.name AS name, AVG(completed_at - started_at) AS average_assistance_time
+FROM assistance_requests
+JOIN students ON (assistance_requests.student_id = students.id)
+JOIN cohorts ON (students.cohort_id = cohorts.id)
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
+
