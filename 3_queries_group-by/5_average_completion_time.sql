96a6933be29e3250e146d2da09a4cf5a0f935537
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-20 01:27:10 +0000

    requirement - average assignment completion time

---
+++ 3_queries_group-by/5_average_completion_time.sql
@@ -0,0 +1,8 @@
+-- get currently enrolled students' average assignment completion time
+
+SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration
+FROM assignment_submissions
+JOIN students ON (assignment_submissions.student_id = students.id)
+WHERE students.end_date IS NULL
+GROUP BY students.name
+ORDER BY average_assignment_duration desc;
