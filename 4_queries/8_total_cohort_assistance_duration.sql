ad704329a5d9e4c18d760407060ca2614cea4294
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-20 19:35:56 +0000

    requirement - total duration of all assistance requests per cohort

---
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -0,0 +1,8 @@
+-- get the total duration of all assistance requests for each cohort
+
+SELECT cohorts.name AS cohort, SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+FROM assistance_requests
+JOIN students ON (assistance_requests.student_id = students.id)
+JOIN cohorts ON (students.cohort_id = cohorts.id)
+GROUP BY cohorts.name
+ORDER BY total_duration;
